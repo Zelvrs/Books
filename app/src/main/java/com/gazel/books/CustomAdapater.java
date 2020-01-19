57a08d16d8d7f3af061922246f256ddf17af93eb
Gazel averrous <Gazel@ngoprekweb.com>
2020-01-19 17:04:08 +0700

    Initial commit

---
+++ app/src/main/java/com/gazel/books/CustomAdapater.java
@@ -0,0 +1,58 @@
+package com.gazel.books;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomAdapater extends ArrayAdapter<Book> {
+
+    private  Context mContext;
+    private  int mResource;
+    private List<Book> mBooks;
+
+    private  static class  ViewHolder {
+        ImageView ivCover;
+    }
+
+    public CustomAdapater(@NonNull Context context, int resource, @NonNull List<Book> books) {
+        super(context, resource, books);
+
+        mContext = context;
+        mResource = resource;
+        mBooks = books;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+
+        ViewHolder viewHolder = new ViewHolder();
+
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(mResource, parent, false);
+
+            viewHolder.ivCover = convertView.findViewById(R.id.ivCover);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        Book book = mBooks.get(position);
+
+        viewHolder.ivCover.setImageResource(book.getCover());
+
+        return convertView;
+    }
+}
